9ae2728dd23e947246d3ad98e26ac162efdf8c26
Андрей Быков <Andrei_93@mail.ru>
2016-02-05 12:56:37 +0300

    Доделать пункт 6 - 10

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,58 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
+        {//пункты 1-4, объявление переменных, ввод значеный, инициализация суммы, вывод значения на экран
+            int x = 13; 
+            int y = 7;
+            int z = x + y;
+            Console.WriteLine(z);
+            //Вывод положительного или отрицательного числа
+            if (z > 0)
+            {
+                Console.WriteLine("Больше нуля");
+            }
+            else
+            {
+                Console.WriteLine("Меньше нуля");
+            }
+            //Четность или не четность значения
+           if (z % 2 == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Не четное");
+            }
+            //проверка делимости на 5
+            if (z % 5 == 0)
+            {
+               Console.WriteLine("Делится на 5");
+            }
+            else
+            {
+               Console.WriteLine("Не делится на 5");
+            }
+            //проверка делимости на 10
+            if (z % 10 == 0)
+            {
+                Console.WriteLine("Делится на 10");
+            }
+            else
+            {
+                Console.WriteLine("Не делится на 10");
+            }
+            //проверка делимости на 20
+             if (z % 20 == 0)
+            {
+                Console.WriteLine("Делится на 20");
+            }
+            else
+            {
+                Console.WriteLine("Не делится на 20");
+            }
+             Console.ReadKey(); 
+          }
         }
     }
-}
+
